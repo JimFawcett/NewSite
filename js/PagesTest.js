e87218d5dde97f5b67ca18aada77a672ea6cad31
Jim Fawcett <fawcettjim@outlook.com>
2024-12-21 16:58:15 -0600

    added content pages

--- js/PagesTest.js
+++ js/PagesTest.js
@@ -11,10 +11,10 @@ function buildPages() {
     "<div class='darkItem menuHeader' onclick='togglePages()'>Thread Pages</div>\
     <div class='menuBody'>\
       <a href='SiteHome.html'>Site Home</a>\
-      <a href='Test1.html'>Test1</a>\
-      <a href='Test2.html'>Test2</a>\
-      <a href='Test3.html'>Test3</a>\
-      <a href='Test4.html'>Test4</a>\
+      <a href='SiteMap.html'>Site Map</a>\
+      <a href='SiteDemo.html'>Site Demo</a>\
+      <a href='SiteDesign.html'>Site Design</a>\
+      <a href='SiteReferences.html'>References</a>\
       <a href='Test5.html'>Test5</a>\
       <a href='Test6.html'>Test6</a>\
       <a href='Test7.html'>Test7</a>\
