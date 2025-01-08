d056c2a75080d1646dbd3edbb8c789cf684a1021
Jim Fawcett <fawcettjim@outlook.com>
2025-01-07 20:49:06 -0600

    adding pages for C++, CSharp, and Python. Rust is nearly done

--- Cpp/js/CppBitesPages.js
+++ Cpp/js/CppBitesPages.js
@@ -13,6 +13,11 @@ function buildPages() {
       <a href='CppBites_Intro.html'>Introduction</a>\
       <a href='CppBites_Hello.html'>Hello</a>\
       <a href='CppBites_Data.html'>Data</a>\
+      <a class='undef' href='javascript:;'>Objects</a>\
+      <a class='undef' href='javascript:;'>Generics</a>\
+      <a class='undef' href='javascript:;'>Iteration</a>\
+      <a href='CppBites_STR.html'>STR</a>\
+      <a href='CppBites_Summary.html'>Summary</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
--- Python/js/PythonBitesPages.js
+++ Python/js/PythonBitesPages.js
@@ -8,10 +8,16 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Thread Pages</div>\
+    "<div class='darkItem menuHeader' onclick='togglePages()'>Python Bites Pages</div>\
     <div class='menuBody'>\
-      <a href='RustBites_Intro.html'>Introduction</a>\
-      <a href='RustBites_Starting.html'>Starting</a>\
+      <a href='PythonBites_Intro.html'>Introduction</a>\
+      <a href='PythonBites_Hello.html'>Hello</a>\
+      <a href='PythonBites_Data.html'>Data</a>\
+      <a class='undef' href='javascript:;'>Objects</a>\
+      <a class='undef' href='javascript:;'>Everything's Generic</a>\
+      <a class='undef' href='javascript:;'>Iteration</a>\
+      <a class='undef' href='javascript:;'>Libraries</a>\
+      <a class='undef' href='javascript:;'>more bites</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
