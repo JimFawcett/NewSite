f867c33ecdea254983ccc739586cd57155cabce0
Jim Fawcett <fawcettjim@outlook.com>
2025-01-29 11:40:15 -0600

    started converting RustStory pages

--- Rust/js/RustStoryPages.js
+++ Rust/js/RustStoryPages.js
@@ -8,9 +8,16 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Thread Pages</div>\
+    "<div class='darkItem menuHeader' onclick='togglePages()'>Rust Story Pages</div>\
     <div class='menuBody'>\
-      <a href='PagePrototype.html'>Page Prototype</a>\
+      <a href='RustStory_Prologue.html'>Introduction</a>\
+      <a href='RustStory_Models.html'>Models</a>\
+      <a href='RustStory_Data.html'>Data</a>\
+      <a href='RustStory_Operations.html'>Operations</a>\
+      <a href='RustStory_Structures.html'>Structures</a>\
+      <a href='RustStory_Libraries.html'>Libraries</a>\
+      <a href='RustStory_References.html'>References</a>\
+      <a href='RustStory_CodeIndex.html'>Code Index</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
