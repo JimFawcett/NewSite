8fbf3a82909162aacdc56a68f7f8ece245d0e8f6
Jim Fawcett <fawcettjim@outlook.com>
2025-01-29 22:54:24 -0600

    fixed transitions, link in Resrcs dropdown

--- Tests/PanelTransitions/js/content.js
+++ Tests/PanelTransitions/js/content.js
@@ -2,8 +2,27 @@
   show and hide left panel in container
 */
 function toggleLeftPanel() {
-  console.log('in toggleLeftPanel');
+  console.warn('in toggleLeftPanel');
   lpanel = document.getElementById('lpanel');
-  console.log('toggling');
+
+  console.warn('toggling');
   lpanel.classList.toggle('hidden');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const lpanel = document.getElementById("lpanel");
+
+  lpanel.addEventListener("transitionstart", function (event) {
+    if (lpanel.classList.contains('hidden')) {
+      lpanel.style.opacity = '0';
+    }
+    console.warn(`Transition started on property: ${event.propertyName}`);
+  });
+
+  lpanel.addEventListener("transitionend", function (event) {
+    if (!lpanel.classList.contains('hidden')) {
+      lpanel.style.opacity = '1';
+    }
+    console.warn(`Transition ended on property: ${event.propertyName}`);
+  });
+});
