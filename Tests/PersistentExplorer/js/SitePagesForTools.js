aba5d626fa90a5a697069ec13faaae4155d57bf9
Jim Fawcett <fawcettjim@outlook.com>
2025-01-31 16:37:46 -0600

    updated panel handling

---
+++ Tests/PersistentExplorer/js/SitePagesForTools.js
@@ -0,0 +1,25 @@
+/*
+ * SitePagesForTool.js - Builds thread page list
+ * ver 1.0 - 13 Jan 2025
+ * Jim Fawcett
+ */
+
+function buildPages() {
+  const pgs = document.getElementById('pages');
+  if(isDefined(pgs)) {
+    pgs.innerHTML =
+    "<div class='darkItem menuHeader' onclick='togglePages()'>Track Pages</div>\
+    <div class='menuBody'>\
+      <a href='../Site/SiteHome.html'>Site Home</a>\
+      <a href='../Rust/RustHome.html'>Rust</a>\
+      <a href='../Cpp/CppHome.html'>C++</a>\
+      <a href='../CSharp/CSharpHome.html'>C#</a>\
+      <a href='../Python/PythonHome.html'>Python</a>\
+      <a href='../WebDev/WebDevHome.html'>WebDev</a>\
+      <a href='../SWDev/SWDevHome.html'>SWDev</a>\
+      <a href='../Basics/BasicsHome.html'>Basics</a>\
+      <a href='../Code/CodeHome.html'>Code</a>\
+      </div>\
+    <div style='height:0.5em;'></div>";
+  }
+}
