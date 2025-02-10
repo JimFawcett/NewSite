e1de6f3c6bc4ea2ed9e36dcd85f2ddb146220b7c
Jim Fawcett <fawcettjim@outlook.com>
2025-02-10 14:33:33 -0600

    created basic demo for Rust

---
+++ Code/Rust/Basics/target/debug/deps/Basics-75a911e74b3ae764.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\libBasics-75a911e74b3ae764.rmeta: src/main.rs
+
+C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\Basics-75a911e74b3ae764.d: src/main.rs
+
+src/main.rs:
---
+++ Code/Rust/Basics/target/debug/deps/Basics.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\NewSite\code\rust\basics\target\debug\deps\Basics.exe: src/main.rs
+
+C:\github\JimFawcett\NewSite\code\rust\basics\target\debug\deps\Basics.d: src/main.rs
+
+src/main.rs:
