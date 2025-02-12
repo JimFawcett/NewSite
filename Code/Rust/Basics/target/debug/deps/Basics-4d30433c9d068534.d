945aa8bf65c1823c412d62399f21eff5afb7c7d8
Jim Fawcett <fawcettjim@outlook.com>
2025-02-11 22:32:34 -0600

    minor code cleanup

--- Code/Rust/Basics/target/debug/deps/Basics-4d30433c9d068534.d
+++
@@ -1,9 +0,0 @@
-C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\libBasics-4d30433c9d068534.rmeta: src/main.rs Cargo.toml
-
-C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\Basics-4d30433c9d068534.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- Code/Rust/Basics/target/debug/deps/Basics.d
+++
@@ -1,5 +0,0 @@
-C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\Basics.exe: src/main.rs
-
-C:\github\JimFawcett\NewSite\Code\Rust\Basics\target\debug\deps\Basics.d: src/main.rs
-
-src/main.rs:
