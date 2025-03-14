691ae68e6c12960e27dc1af26e1e00f53eaefb1f
Jim Fawcett <fawcettjim@outlook.com>
2025-03-14 07:36:02 -0500

    minor style and content changes

--- Basics/archive/js/BasicsBitesPages.js
+++ Basics/archive/js/BasicsBitesPages.js
@@ -8,13 +8,12 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Basics Track Pages</div>\
-    <div class='menuBody'>\
-      <a href='BasicsHome.html'>Track Summary</a>\
-      <a class='undef' href='javascript:;'>Introduction</a>\
-      <a class='undef' href='javascript:;'>Platform</a>\
-      <a class='undef' href='javascript:;'>Platform memory</a>\
-      <a class='undef' href='javascript:;'>many more</a>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Basics Track Pages</div>\
+      <div class='menuBody'>\
+        <a href='Basics_Platform.html'>Platform</a>\
+        <a href='Basics_Memory.html'>Virtual memory</a>\
+        <a class='undef' href='javascript:;'>Scheduling</a>\
+        <a class='undef' href='javascript:;'>IO</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
