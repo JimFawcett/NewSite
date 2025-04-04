dccac71d5b10823fb8f882586ac9c2250ccc2ee2
Jim Fawcett <fawcettjim@outlook.com>
2025-04-04 10:18:56 -0500

    changed code button top menu to open GridExplorer

--- js/BlogsAndHelp.js
+++ js/BlogsAndHelp.js
@@ -177,7 +177,7 @@ function buildRes() {
       <div class='category'>-Tests-</div>\
       <details style='margin-left:0.5rem;'>\
         <summary>Web Tech Tests</summary>\
-        <a target='_top' href='../Tests/GridExplorer/PrototypeExplore.html'>GridExplorer</a>\
+        <a target='_top' href='../Tests/GridExplorer/GridExplorer.html'>GridExplorer</a>\
         <a target='_top' href='../Tests/FigureSizer/ImageSizer.html'>FigureSizer</a>\
         <a target='_top' href='../Tests/link-navigator/LinkNavigator.html'>LinkNavigator</a>\
         <a target='_top' href='../Tests/PopUpWindow/PopUp.html'>PopUpWindow</a>\
