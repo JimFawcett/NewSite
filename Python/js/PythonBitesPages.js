bf69260f4b16db1385472a5dac0197b563403047
Jim Fawcett <fawcettjim@outlook.com>
2025-04-21 13:32:51 -0500

    added python bites

--- Python/js/PythonBitesPages.js
+++ Python/js/PythonBitesPages.js
@@ -8,10 +8,10 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Python Bites Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Python Bites Pages</div>\
     <div class='menuBody'>\
       <a href='PythonBites_Intro.html'>Introduction</a>\
-      <a href='PythonBites_Hello.html'>Hello</a>\
+      <a href='PythonBites_HelloPython.html'>Hello</a>\
       <a href='PythonBites_Data.html'>Data</a>\
       <a class='undef' href='javascript:;'>Objects</a>\
       <a class='undef' href='javascript:;'>Everything's Generic</a>\
