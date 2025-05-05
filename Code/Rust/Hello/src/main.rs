9c59116530a2ea80b866bf7692aad0bf6ef0abaa
Jim Fawcett <fawcettjim@outlook.com>
2025-05-05 16:17:53 -0500

    cleaned up text and diagram in RustHome

---
+++ Code/Rust/Hello/src/main.rs
@@ -0,0 +1,8 @@
+/*-----------------------------------------------
+  Rust Hello World
+*/
+fn main() {
+  let s:String = "Hello Rust World".to_string();
+  println!("\n  {s:?}");
+  println!("\n  -- That\'s all Folks! --\n");
+}
---
+++ Code/Rust/Hello/target/debug/Hello.d
@@ -0,0 +1,1 @@
+C:\github\JimFawcett\NewSite\Code\Rust\Hello\target\debug\Hello.exe: C:\github\JimFawcett\NewSite\Code\Rust\Hello\src\main.rs
