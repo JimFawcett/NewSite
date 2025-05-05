0f9349927fac38ee7937c81990223e4a48be5f87
Jim Fawcett <fawcettjim@outlook.com>
2025-05-05 16:19:23 -0500

    removed build artifacts from Rust\Hello

--- Code/Rust/Hello/target/debug/Hello.d
+++
@@ -1,1 +0,0 @@
-C:\github\JimFawcett\NewSite\Code\Rust\Hello\target\debug\Hello.exe: C:\github\JimFawcett\NewSite\Code\Rust\Hello\src\main.rs
--- Code/Rust/Hello/target/debug/deps/Hello.d
+++
@@ -1,5 +0,0 @@
-C:\github\JimFawcett\NewSite\Code\Rust\Hello\target\debug\deps\Hello.exe: src\main.rs
-
-C:\github\JimFawcett\NewSite\Code\Rust\Hello\target\debug\deps\Hello.d: src\main.rs
-
-src\main.rs:
