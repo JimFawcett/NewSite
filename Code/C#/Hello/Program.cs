6bd578661b4d912312757e6c7613a54d2528e6b9
Jim Fawcett <fawcettjim@outlook.com>
2025-05-09 11:41:52 -0500

    updated C# Track Summary and first bite

---
+++ Code/C#/Hello/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n  Hello C# World");
+        Console.WriteLine("\n  -- That's all Folks --\n");
+    }
+}
