57e6af3f178c42fabf962a5341d68ce24421a1a8
Jim Fawcett <fawcettjim@outlook.com>
2025-05-29 18:55:23 -0500

    added Basic_Plans page

--- Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
+++ Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c715d14e91033b9c3bc7703ae8fc45aadbf31723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d3bc669d178caf2054fbe14a89a8e4be9976e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
