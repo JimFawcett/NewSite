f595da8fe046ed7a387f3a9da59bc1e9cfc97879
Jim Fawcett <fawcettjim@outlook.com>
2025-06-05 11:40:53 -0500

    adding languages page to Basic Bites

--- Basics/js/BasicsBitesPages.js
+++ Basics/js/BasicsBitesPages.js
@@ -16,6 +16,7 @@ function buildPages() {
         <a href='Basics_Processes.html'>Processes</a>\
         <a href='Basics_IO.html'>IO</a>\
         <a href='Basics_SysRescr.html'>System Resources</a>\
+        <a href='Basics_Languages.html'>Languages</a>\
         <a href='Basics_Plans.html'>Plans</a>\
       </div>\
     <div style='height:0.5em;'></div>";
