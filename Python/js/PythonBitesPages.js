39abf8b44a47092bc735c40a7bfa66917a50005a
Jim Fawcett <fawcettjim@outlook.com>
2025-06-06 15:40:20 -0500

    Fixed links, minor style changes to explorer styles

--- Python/js/PythonBitesPages.js
+++ Python/js/PythonBitesPages.js
@@ -17,6 +17,7 @@ function buildPages() {
       <a class='undef' href='javascript:;'>Everything's Generic</a>\
       <a class='undef' href='javascript:;'>Iteration</a>\
       <a class='undef' href='javascript:;'>Libraries</a>\
+      <a href='PythonRepos.html'>Repositories</a>\
       <a class='undef' href='javascript:;'>more bites</a>\
       </div>\
     <div style='height:0.5em;'></div>";
