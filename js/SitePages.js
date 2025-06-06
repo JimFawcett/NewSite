d040f2add0bf80eb732af874ea154bd7c5d47560
Jim Fawcett <fawcettjim@outlook.com>
2025-06-06 15:40:20 -0500

    Fixed links, minor style changes to explorer styles

--- js/SitePages.js
+++ js/SitePages.js
@@ -8,7 +8,7 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Track Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Site Pages</div>\
     <div class='menuBody'>\
       <a href='SiteMap.html'>Site Map</a>\
       <a href='SiteDemo.html'>Site Demo</a>\
--- js/SitePagesForTools.js
+++ js/SitePagesForTools.js
@@ -8,7 +8,7 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem listheader' onclick='togglePages()'>Track Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Track Summaries</div>\
     <div class='menuBody'>\
       <a href='../Site/SiteHome.html'>Site Home</a>\
       <a href='../Rust/RustHome.html'>Rust</a>\
