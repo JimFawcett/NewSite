e0da272ccbe4cab603b705e49a5e030643d59ea6
Jim Fawcett <fawcettjim@outlook.com>
2025-06-18 08:37:44 -0500

    ported C# data and objects

--- CSharp/js/CSharpBitesPages.js
+++ CSharp/js/CSharpBitesPages.js
@@ -14,10 +14,9 @@ function buildPages() {
       <a href='CSharpBites_Execution.html'>Execution</a>\
       <a href='CSharpBites_HelloCSharp.html'>Hello</a>\
       <a href='CSharpBites_Data.html'>Data</a>\
-      <a class='undef' href='javascript:;'>Objects</a>\
+      <a href='CSharpBites_Objects.html'>Objects</a>\
       <a class='undef' href='javascript:;'>Generics</a>\
       <a class='undef' href='javascript:;'>Iteration</a>\
-      <a class='undef' href='javascript:;'>Execution</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
