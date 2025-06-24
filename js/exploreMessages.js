79f0d68b544068e586a27d89a382e88a0b05bc16
Jim Fawcett <fawcettjim@outlook.com>
2025-06-23 22:21:27 -0500

    make clear persistent

--- js/exploreMessages.js
+++ js/exploreMessages.js
@@ -41,22 +41,37 @@ function clearMenus() {
   let blgs = document.getElementById('blogs');
   if(isDefined(blgs)) {
     blgs.classList.add('hidden');
+    setCookie('blogs', false, 10);
   }
   let hlp = document.getElementById('help');
   if(isDefined(hlp)) {
     hlp.classList.add('hidden');
+    setCookie('help', false, 10);
   }
   let rcs = document.getElementById('res');
   if(isDefined(rcs)) {
     rcs.classList.add('hidden');
+    setCookie('res', false, 10);
+  }
+  let pages = document.getElementById('pages');
+  if(isDefined(pages)) {
+    pages.classList.add('hidden');
+    setCookie('pages', false, 10);
+  }
+  let sections = document.getElementById('sections');
+  if(isDefined(sections)) {
+    sections.classList.add('hidden');
+    setCookie('sections', false, 10);
   }
   let tpi = document.getElementById('top-menu-info');
   if(isDefined(tpi)) {
     tpi.classList.add('hidden');
+    setCookie('top-menu-info', false, 10);
   }
   let bpi = document.getElementById('bottom-menu-info');
   if(isDefined(bpi)) {
     bpi.classList.add('hidden');
+    setCookie('bottom-menu-info', false, 10);
   }
   postMsg(makeMsg('clear', null));
 }
