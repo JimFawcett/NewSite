dc2c11fae394c4cfb4d3c2741fbfe14454ca8606
Jim Fawcett <fawcettjim@outlook.com>
2025-06-26 21:04:01 -0500

    added change and glossary pages

--- WebDev/js/WebDevBitesPages.js
+++ WebDev/js/WebDevBitesPages.js
@@ -8,11 +8,11 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>WebDev Bites Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>WebDev Bites Pages</div>\
     <div class='menuBody'>\
-      <a href='WebDevHome.html'>Track Summary</a>\
       <a class='undef' href='javascript:;'>Introduction</a>\
       <a class='undef' href='javascript:;'>more</a>\
+      <a href='WebDevBites_Glossary.html'>Glossary</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
