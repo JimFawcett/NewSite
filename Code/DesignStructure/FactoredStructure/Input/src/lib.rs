e0f762855e9c810b6abd690c0f27bf8ed377fd08
Jim Fawcett <fawcettjim@outlook.com>
2025-07-03 20:42:29 -0500

    restored DesignStructure directory

---
+++ Code/DesignStructure/FactoredStructure/Input/src/lib.rs
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////
+// FactoredStructure::Input::lib.rs                        //
+//   - Input attempts to open named file and return File   //
+// Jim Fawcett, https://JimFawcett.github.io, 04 Mar 2021  //
+/////////////////////////////////////////////////////////////
+
+use std::fs::*;
+
+mod file_utilities;
+use file_utilities::open_file_for_read;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Input {
+  name: String,
+}
+impl Input {
+  pub fn new() -> Input {
+    Input {
+      name: String::new(),
+    }
+  }
+  pub fn do_input(&mut self, name: &str) -> Option<File> {
+    let rslt = open_file_for_read(name);
+    if let Ok(file) = rslt {
+      return Some(file);
+    } else {
+      print!("\n  can't open file {:?}", name);
+      return None;
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_works() {
+    assert_eq!(2 + 2, 4);
+  }
+}
