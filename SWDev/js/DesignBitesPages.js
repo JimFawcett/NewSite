b147c536dd4695e75dfb52d83e741f101b30155e
Jim Fawcett <fawcettjim@outlook.com>
2025-07-06 11:45:48 -0500

    updated links

--- SWDev/js/DesignBitesPages.js
+++ SWDev/js/DesignBitesPages.js
@@ -8,7 +8,7 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem listheader' onclick='togglePages()'>SWDev Bites Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>SW Design Bites Pages</div>\
     <div class='menuBody'>\
       <a href='SWDesignBites_Intro.html'>Introduction</a>\
       <a href='SWDesignBites_Structure.html'>Structure</a>\
