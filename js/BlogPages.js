8b244e9159fc91fa62c53f63a518ef071c755a7a
Jim Fawcett <fawcettjim@outlook.com>
2025-07-09 11:26:48 -0500

    adding RustBites pages

--- js/BlogPages.js
+++ js/BlogPages.js
@@ -11,6 +11,7 @@ function buildPages() {
     "<div class='darkItem listheader' onclick='togglePages()'>Blog Pages</div>\
     <div class='menuBody'>\
       <a href='Blog.html'>First Things</a>\
+      <a href='Blog_DisthinguishedRust.html'>Disthinguished Rust</a>\
       <a href='Blog_CommCompare.html'>Comm Compare</a>\
       </div>\
     <div style='height:0.5em;'></div>";
