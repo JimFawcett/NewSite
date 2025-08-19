e403d42bd22c8cdfc1ac89505e594238ee5aeb17
Jim Fawcett <fawcettjim@outlook.com>
2025-08-19 17:50:28 -0500

    working on zoom

--- Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
+++ Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15bdb3a7519b70a3545bc57d7428aa7207a08a50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f43e03d7e82d4ed303ef0c77ad2907688de8bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
