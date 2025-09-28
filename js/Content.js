e32ca8d87a7ec7805fca5ad7b6ee30f90678b851
Jim Fawcett <fawcettjim@outlook.com>
2025-09-28 17:17:29 -0500

    minor tweaks to font-size, font-family

--- js/Content.js
+++ js/Content.js
@@ -174,139 +174,139 @@ function makeMsg(key, value) {
 
   - NOTE: overridden by handler in contentMessages.js
 */
-window.onmessage = function (e) {
-  console.log('in msg loop: msg = ' + e.data);
-  switch (e.data) {
-    case 'about':
-      toggleButton('about');
-      break;
-    case 'keys':
-      toggleButton('keys');
-      break;
-    case 'next':
-      let nxt = document.getElementById('next');
-      if(isDefined(nxt)) {
-        nxt.click();
-      }
-      break;
-    case 'prev':
-      let prv = document.getElementById('prev');
-      if(isDefined(prv)) {
-        prv.click();
-      }
-      break;
-      case 'compare':
-        hideElement('pages');
-        toggleCompare();
-        break;
-      case 'sections':
-        let pgs = document.getElementById('pages');
-        let scs = document.getElementById('sections');
-        if(isDefined(pgs)) {
-          hideElement('pages');
-        }
-        if(isDefined(scs)) {
-          //toggleButton('sections');
-          toggleElement('sections');
-        }
-        break;
-      case 'pages':
-        let pgs2 = document.getElementById('pages');
-        let scs2 = document.getElementById('sections');
-        if(isDefined(scs2)) {
-          hideElement('sections');
-        }
-        if(isDefined(pgs2)) {
-          toggleElement('pages');
-        }
-        break;
-      case 'clear':
-        clearMenues();
-        break;
-    /* Explorer cases require files in NewSite or an immediate child */
-    case 'Explore':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/Explore.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../Explore.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreCode':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/ExploreCode.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../ExploreCode.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreRust':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/Rust/ExploreRust.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../Rust/ExploreRust.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreCpp':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/Cpp/ExploreCpp.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../Cpp/ExploreCpp.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreCSharp':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/CSharp/ExploreCSharp.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../CSharp/ExploreCSharp.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExplorePython':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/Python/ExplorePython.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../Python/ExplorePython.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreSWDev':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/SWDev/ExploreSWDev.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../SWDev/ExploreSWDev.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreWebDev':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/WebDev/ExploreWebDev.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../WebDev/ExploreWebDev.html?src=' + window.location.href;
-      }
-      break;
-    case 'ExploreBasics':
-      if(dirName() === "NewSite") {
-        window.top.location.href = '../NewSite/Basics/ExploreBasics.html?src=' + window.location.href;
-      }
-      else {
-        window.top.location.href = '../Basics/ExploreBasics.html?src=' + window.location.href;
-      }
-      break;    
-    case 'goto':
-      alert('GoTo msg');
-      showGoToMenu(); 
-      break; 
-    case 'close-goto':
-      alert('close-goto');
-      hideElement('goto');                                                
-    default:
+// window.onmessage = function (e) {
+//   console.log('in msg loop: msg = ' + e.data);
+//   switch (e.data) {
+//     case 'about':
+//       toggleButton('about');
+//       break;
+//     case 'keys':
+//       toggleButton('keys');
+//       break;
+//     case 'next':
+//       let nxt = document.getElementById('next');
+//       if(isDefined(nxt)) {
+//         nxt.click();
+//       }
+//       break;
+//     case 'prev':
+//       let prv = document.getElementById('prev');
+//       if(isDefined(prv)) {
+//         prv.click();
+//       }
+//       break;
+//       case 'compare':
+//         hideElement('pages');
+//         toggleCompare();
+//         break;
+//       case 'sections':
+//         let pgs = document.getElementById('pages');
+//         let scs = document.getElementById('sections');
+//         if(isDefined(pgs)) {
+//           hideElement('pages');
+//         }
+//         if(isDefined(scs)) {
+//           //toggleButton('sections');
+//           toggleElement('sections');
+//         }
+//         break;
+//       case 'pages':
+//         let pgs2 = document.getElementById('pages');
+//         let scs2 = document.getElementById('sections');
+//         if(isDefined(scs2)) {
+//           hideElement('sections');
+//         }
+//         if(isDefined(pgs2)) {
+//           toggleElement('pages');
+//         }
+//         break;
+//       case 'clear':
+//         clearMenues();
+//         break;
+//     /* Explorer cases require files in NewSite or an immediate child */
+//     case 'Explore':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/Explore.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../Explore.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreCode':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/ExploreCode.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../ExploreCode.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreRust':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/Rust/ExploreRust.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../Rust/ExploreRust.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreCpp':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/Cpp/ExploreCpp.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../Cpp/ExploreCpp.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreCSharp':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/CSharp/ExploreCSharp.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../CSharp/ExploreCSharp.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExplorePython':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/Python/ExplorePython.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../Python/ExplorePython.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreSWDev':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/SWDev/ExploreSWDev.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../SWDev/ExploreSWDev.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreWebDev':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/WebDev/ExploreWebDev.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../WebDev/ExploreWebDev.html?src=' + window.location.href;
+//       }
+//       break;
+//     case 'ExploreBasics':
+//       if(dirName() === "NewSite") {
+//         window.top.location.href = '../NewSite/Basics/ExploreBasics.html?src=' + window.location.href;
+//       }
+//       else {
+//         window.top.location.href = '../Basics/ExploreBasics.html?src=' + window.location.href;
+//       }
+//       break;    
+//     case 'goto':
+//       alert('GoTo msg');
+//       showGoToMenu(); 
+//       break; 
+//     case 'close-goto':
+//       alert('close-goto');
+//       hideElement('goto');                                                
+//     default:
 
-  }
-}
+//   }
+// }
 function showGoToMenu() {
   // alert('goto');
   let gte = document.getElementById('goto');
