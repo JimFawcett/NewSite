ee8d017e65e3addd131dac82872f8261a97e3d80
Jim Fawcett <fawcettjim@outlook.com>
2025-10-08 12:49:15 -0500

    fixed styles precedence, added content

--- WebDev/js/WebDevCompPages.js
+++ WebDev/js/WebDevCompPages.js
@@ -10,6 +10,9 @@ function buildPages() {
     pgs.innerHTML =
     "<div class='darkItem listheader' onclick='togglePages()'>WebDev Comp Pages</div>\
     <div class='menuBody'>\
+      <a href='WebDevBites_CompSpacer.html'>Spacer Demo</a>\
+      <a href='WebDevBites_CompSpacerDesign.html'>Spacer Design</a>\
+      <a href='WebDevBites_CompSpacerInterface.html'>Spacer Interface</a>\
       <a href='WebDevBites_CompImageViewer.html'>ImageView Demo</a>\
       <a href='WebDevBites_CompImageViewerDesign.html'>ImageView Design</a>\
       <a href='WebDevBites_CompImageViewerInterface.html'>ImageView Interface</a>\
