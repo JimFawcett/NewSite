035d86ba3e9e464de235c11b03791611c7512cc5
Jim Fawcett <fawcettjim@outlook.com>
2025-12-03 20:40:16 -0600

    fixed top-menu styles

--- Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
+++ Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3909f2e05c0ed7856c989e8c9620754a7b00550b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d58c58031b7d921b1edf9de5b55a8357b0af4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
