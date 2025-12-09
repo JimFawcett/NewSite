0fe7aadbfb2efa022987a5fca29ef0ad74dffa47
Jim Fawcett <fawcettjim@outlook.com>
2025-12-08 19:45:56 -0600

    adding pages to Code Track

---
+++ Code/js/CodeBitesPages.js
@@ -0,0 +1,17 @@
+/*
+ * PagesTest.js - Builds thread page list
+ * ver 1.0 - 19 Dec 2024
+ * Jim Fawcett
+ */
+
+function buildPages() {
+  const pgs = document.getElementById('pages');
+  if(isDefined(pgs)) {
+    pgs.innerHTML =
+    "<div class='darkItem listheader' onclick='togglePages()'>Code Bites Pages</div>\
+      <div class='menuBody'>\
+        <a href='CodeBites_UseAI.html'>Use AI</a>\
+      </div>\
+    <div style='height:0.5em;'></div>";
+  }
+}
