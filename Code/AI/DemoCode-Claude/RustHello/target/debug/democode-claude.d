da35898959ed5fee6219d0b23b214e5ef1005791
Jim Fawcett <fawcettjim@outlook.com>
2026-01-24 20:46:53 -0600

    added Bite for AI Consoles

---
+++ Code/AI/DemoCode-Claude/RustHello/target/debug/democode-claude.d
@@ -0,0 +1,1 @@
+C:\github\JimFawcett\NewSite\Code\AI\DemoCode-Claude\RustHello\target\debug\democode-claude.exe: C:\github\JimFawcett\NewSite\Code\AI\DemoCode-Claude\RustHello\src\main.rs
---
+++ Code/AI/DemoCode-Claude/RustHello/target/debug/deps/democode_claude.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\NewSite\Code\AI\DemoCode-Claude\RustHello\target\debug\deps\democode_claude.d: src\main.rs
+
+C:\github\JimFawcett\NewSite\Code\AI\DemoCode-Claude\RustHello\target\debug\deps\democode_claude.exe: src\main.rs
+
+src\main.rs:
