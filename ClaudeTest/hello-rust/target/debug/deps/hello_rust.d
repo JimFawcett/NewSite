9ee02e3d6641b0e38564b82a7397d47e5239777c
Jim Fawcett <fawcettjim@outlook.com>
2026-02-08 20:28:28 -0600

    added claudetest directory

--- ClaudeTest/hello-rust/target/debug/deps/hello_rust.d
+++
@@ -1,5 +0,0 @@
-C:\github\Jimfawcett\NewSite\ClaudeTest\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
-
-C:\github\Jimfawcett\NewSite\ClaudeTest\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
-
-src\main.rs:
--- ClaudeTest/hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-C:\github\Jimfawcett\NewSite\ClaudeTest\hello-rust\target\debug\hello-rust.exe: C:\github\Jimfawcett\NewSite\ClaudeTest\hello-rust\src\main.rs
