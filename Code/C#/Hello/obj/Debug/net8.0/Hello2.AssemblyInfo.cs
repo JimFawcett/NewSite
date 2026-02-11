48ddb56c6f0615dd4e5a0846b92cd97637c1a703
Jim Fawcett <fawcettjim@outlook.com>
2026-02-11 10:56:52 -0600

    modified text and diagrams

--- Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
+++ Code/C#/Hello/obj/Debug/net8.0/Hello2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6078ae574989cc13b4f0d678ca806892829705b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee02e3d6641b0e38564b82a7397d47e5239777c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
