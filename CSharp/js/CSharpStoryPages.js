a8ab51cc5cb3caa58bc7c6a23fda432635c755d2
Jim Fawcett <fawcettjim@outlook.com>
2026-04-26 18:42:58 -0500

    added Python story

--- CSharp/js/CSharpStoryPages.js
+++ CSharp/js/CSharpStoryPages.js
@@ -18,10 +18,11 @@ function buildPages() {
       <a href='CSharpStory_Classes.html'>Chap 5 - Classes</a>\
       <a href='CSharpStory_ClassRelationships.html'>Chap 6 - Class Relships</a>\
       <a href='CSharpStory_Generics.html'>Chap 7 - Generics</a>\
-      <a href='CSharpStory_Libraries.html'>Chap 8 - Libraries</a>\
-      <a href='CSharpStory_LibraryStreams.html'>Chap 9 - Streams</a>\
-      <a href='CSharpStory_LibraryCollections.html'>Chap 10 - Collections</a>\
-      <a href='CSharpStory_Interesting.html'>Chap 11 - Interesting</a>\
+      <a href='CSharpStory_Reflection.html'>Chap 8 - Reflection</a>\
+      <a href='CSharpStory_Libraries.html'>Chap 9 - Libraries</a>\
+      <a href='CSharpStory_LibraryStreams.html'>Chap 10 - Streams</a>\
+      <a href='CSharpStory_LibraryCollections.html'>Chap 11 - Collections</a>\
+      <a href='CSharpStory_Interesting.html'>Chap 12 - Interesting</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
--- Python/js/PythonStoryPages.js
+++ Python/js/PythonStoryPages.js
@@ -1,6 +1,6 @@
 /*
- * PagesTest.js - Builds thread page list
- * ver 1.0 - 19 Dec 2024
+ * PythonStoryPages.js - Builds Python Story page list
+ * ver 1.0 - 26 Apr 2026
  * Jim Fawcett
  */
 
@@ -8,9 +8,20 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Thread Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Story Pages</div>\
     <div class='menuBody'>\
-      <a href='PagePrototype.html'>Page Prototype</a>\
+      <a href='PythonStory_Prologue.html'>Prologue</a>\
+      <a href='PythonStory_Models.html'>Chap 1 - Models</a>\
+      <a href='PythonStory_Survey.html'>Chap 2 - Survey</a>\
+      <a href='PythonStory_Data.html'>Chap 3 - Data</a>\
+      <a href='PythonStory_Operations.html'>Chap 4 - Operations</a>\
+      <a href='PythonStory_Classes.html'>Chap 5 - Classes</a>\
+      <a href='PythonStory_ClassRelationships.html'>Chap 6 - Class Relships</a>\
+      <a href='PythonStory_Generics.html'>Chap 7 - Generics</a>\
+      <a href='PythonStory_Libraries.html'>Chap 8 - Libraries</a>\
+      <a href='PythonStory_LibraryIO.html'>Chap 9 - I/O Library</a>\
+      <a href='PythonStory_LibraryCollections.html'>Chap 10 - Collections</a>\
+      <a href='PythonStory_Interesting.html'>Chap 11 - Interesting</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
