7f873bc46a19c8ee76267d9fa14ede13ecda7683
Jim Fawcett <fawcettjim@outlook.com>
2026-04-29 13:01:27 -0500

    added Basics story

--- Basics/js/BasicsStoryPages.js
+++ Basics/js/BasicsStoryPages.js
@@ -1,6 +1,6 @@
 /*
- * PagesTest.js - Builds thread page list
- * ver 1.0 - 19 Dec 2024
+ * BasicsStoryPages.js - Builds Basics Story page list
+ * ver 1.0 - 29 Apr 2026
  * Jim Fawcett
  */
 
@@ -8,9 +8,16 @@ function buildPages() {
   const pgs = document.getElementById('pages');
   if(isDefined(pgs)) {
     pgs.innerHTML =
-    "<div class='darkItem menuHeader' onclick='togglePages()'>Thread Pages</div>\
+    "<div class='darkItem listheader' onclick='togglePages()'>Basics Story Pages</div>\
     <div class='menuBody'>\
-      <a href='PagePrototype.html'>Page Prototype</a>\
+      <a href='BasicsStory_Prologue.html'>Prologue</a>\
+      <a href='BasicsStory_Hardware.html'>Chap 1 - Hardware</a>\
+      <a href='BasicsStory_Software.html'>Chap 2 - Software</a>\
+      <a href='BasicsStory_Languages.html'>Chap 3 - Languages</a>\
+      <a href='BasicsStory_Tooling.html'>Chap 4 - Tooling</a>\
+      <a href='BasicsStory_DataStructures.html'>Chap 5 - Data Structures</a>\
+      <a href='BasicsStory_Concurrency.html'>Chap 6 - Concurrency</a>\
+      <a href='BasicsStory_Deployment.html'>Chap 7 - Deployment</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
--- SWDev/js/SWDevBitesPages.js
+++ SWDev/js/SWDevBitesPages.js
@@ -1,6 +1,6 @@
 /*
- * PagesTest.js - Builds thread page list
- * ver 1.0 - 19 Dec 2024
+ * SWDevBitesPages.js - Builds SWDev bites page list
+ * ver 1.1 - 29 Apr 2026
  * Jim Fawcett
  */
 
@@ -10,9 +10,26 @@ function buildPages() {
     pgs.innerHTML =
     "<div class='darkItem listheader' onclick='togglePages()'>SWDev Bites Pages</div>\
     <div class='menuBody'>\
-      <a class='undef' href='javascript:;'>Introduction</a>\
-      <a class='undef' href='javascript:;'>more</a>\
+      <div class='darkItem' style='padding-left:0.5rem; font-size:0.9rem'>Design</div>\
+      <a href='SWDesignBites_Intro.html'>Intro</a>\
+      <a href='SWDesignBites_Structure.html'>Structure</a>\
+      <a href='SWDesignBites_StructureBasic.html'>Monolithic</a>\
+      <a href='SWDesignBites_StructureFactored.html'>Factored</a>\
+      <a href='SWDesignBites_StructureDataFlow.html'>Data Flow</a>\
+      <a href='SWDesignBites_StructureTypeErase.html'>Type Erase</a>\
+      <a href='SWDesignBites_StructurePlugin.html'>Plugin</a>\
+      <div class='darkItem' style='padding-left:0.5rem; font-size:0.9rem'>Deploy</div>\
+      <a href='SWDeployBites_Intro.html'>Intro</a>\
+      <a href='SWDeployBites_Process.html'>Process</a>\
+      <a href='SWDeployBites_Git.html'>Config Mgmt</a>\
+      <a class='undef' href='javascript:;'>Scripting</a>\
+      <a class='undef' href='javascript:;'>Containers</a>\
+      <a class='undef' href='javascript:;'>Platforms</a>\
+      <a class='undef' href='javascript:;'>Publication</a>\
+      <a class='undef' href='javascript:;'>Use Cases</a>\
+      <div class='darkItem' style='padding-left:0.5rem; font-size:0.9rem'>General</div>\
       <a href='SWDevBites_Glossary.html'>Glossary</a>\
+      <a href='SWDevReferences.html'>References</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
