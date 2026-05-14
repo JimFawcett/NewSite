ea633d06d93b14154cf542b71820dde425187828
Jim Fawcett <fawcettjim@outlook.com>
2026-05-14 08:30:38 -0500

    added new entries to Rust Glossary

--- Code/C#/Hello/obj/Debug/net10.0/Hello2.AssemblyInfo.cs
+++ Code/C#/Hello/obj/Debug/net10.0/Hello2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abcf321c24e2a6400eb83ade6ad87ac4ea3f0841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aae72b07ae5da3e5ae79ace6351db7eebe8b9b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
