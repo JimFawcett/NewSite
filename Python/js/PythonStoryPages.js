125273b6dec587f793a06453395c4f0bf5e8b248
Jim Fawcett <fawcettjim@outlook.com>
2026-05-15 22:15:23 -0500

    added PythonStory page for 3rd party libraries

--- Python/js/PythonStoryPages.js
+++ Python/js/PythonStoryPages.js
@@ -21,7 +21,8 @@ function buildPages() {
       <a href='PythonStory_Libraries.html'>Chap 8 - Libraries</a>\
       <a href='PythonStory_LibraryIO.html'>Chap 9 - I/O Library</a>\
       <a href='PythonStory_LibraryCollections.html'>Chap 10 - Collections</a>\
-      <a href='PythonStory_Interesting.html'>Chap 11 - Interesting</a>\
+      <a href='PythonStory_3rdPartyLibraries.html'>Chap 11 - 3rd Party Libs</a>\
+      <a href='PythonStory_Interesting.html'>Chap 12 - Interesting</a>\
       </div>\
     <div style='height:0.5em;'></div>";
   }
