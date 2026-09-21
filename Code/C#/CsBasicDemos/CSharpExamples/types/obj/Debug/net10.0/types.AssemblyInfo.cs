5c9bf814a92f570a033d817bc3fb08c45730ad81
Jim Fawcett <fawcettjim@outlook.com>
2026-09-21 11:52:54 -0500

    minor edits to document web pages

--- Code/C#/CsBasicDemos/CSharpExamples/types/obj/Debug/net10.0/types.AssemblyInfo.cs
+++ Code/C#/CsBasicDemos/CSharpExamples/types/obj/Debug/net10.0/types.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("types")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b412984084766c314ca1d39aefea23745e01f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+585e43465a31798dec19ee57a34c3ba8ed3a3a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("types")]
 [assembly: System.Reflection.AssemblyTitleAttribute("types")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
